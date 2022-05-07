c54ae6fe5481816dcf285d37133b6e77f0178923
OUSS <ousamalaraba@gmail.com>
2022-05-07 10:35:57 +0100

    deleting the conflict
    Please enter the codmmit message for your changes. Lines starting

--- blog/apps.py
+++ blog/apps.py
@@ -4,6 +4,4 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
-    
-    
-# i am a comment please delete me i will cause you conflicts
+#i am deleting you know

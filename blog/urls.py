81e69cdc683c408b8f9585b2f78388706f99333b
OUSS <ousamalaraba@gmail.com>
2022-05-07 22:46:07 +0100

    part 10 CRUD operations

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from django.urls.conf import include
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView)
 from . import views
 
 urlpatterns = [
     # for better organisation you have to name it by his package, in place of just home we name it blog-home
-    path('', views.home, name="blog-home"),
-    path('about/', views.about, name="blog-about"), 
+    # path('', views.home, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),# we gonna use the same template for updating so the class view expecte post_form
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
+    path('about/', views.about, name="blog-about"),
 ]

2144dcbb4c1961a017e1f31a6a4f4975153cd142
OUSS <ousamalaraba@gmail.com>
2022-05-10 01:35:33 +0100

    finish part 11 pagination

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,15 @@ from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
-                    PostDeleteView)
+                    PostDeleteView,
+                    UserPostListView)
 from . import views
 
 urlpatterns = [
     # for better organisation you have to name it by his package, in place of just home we name it blog-home
     # path('', views.home, name="blog-home"),
     path('', PostListView.as_view(), name="blog-home"),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/new/', PostCreateView.as_view(), name="post-create"),# we gonna use the same template for updating so the class view expecte post_form
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
